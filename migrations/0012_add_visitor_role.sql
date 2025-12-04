4aa024bc3714081c6ee2bfd3a0f66bde78ee15fd
Okolie Amuche Anthony <128842118+king-tony01@users.noreply.github.com>
2025-12-04 18:48:08 +0100

    Update 0012_add_visitor_role.sql
    Changed the migration format identifier

--- migrations/0012_add_visitor_role.sql
+++ migrations/0012_add_visitor_role.sql
@@ -1,5 +1,5 @@
--- Up
+-- UP
 ALTER TABLE users MODIFY COLUMN user_role ENUM ('student', 'developer', 'staff', 'visitor') NOT NULL DEFAULT 'student';
 
--- Down
+-- DOWN
 ALTER TABLE users MODIFY COLUMN user_role ENUM ('student', 'developer', 'staff') NOT NULL DEFAULT 'student';

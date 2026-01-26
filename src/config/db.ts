62f7355c5bb45f516bd9e105ab0ecc68400f15a8
KingTony Technologies <kingtony3825@gmail.com>
2026-01-26 17:23:17 +0100

    changed db connection method to use socketPath instead

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,8 +4,9 @@ const pool: Pool = createPool({
   user: process.env.DB_USER!,
   password: process.env.DB_PASSWORD!,
   database: process.env.DB_NAME!,
-  host: process.env.DB_HOST!,
-  port: Number(process.env.DB_PORT!),
+  socketPath: process.env.DB_HOST!,
+  // host: process.env.DB_HOST!,
+  // port: Number(process.env.DB_PORT!),
   // ssl: false,
 });
 
